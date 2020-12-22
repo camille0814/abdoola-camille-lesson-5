bcd4e1c1ffe8788e48121ad4cd46040d964a173f
camille0814 <72763933+camille0814@users.noreply.github.com>
2020-12-22 09:26:32 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print("Light Level:" + input.light_level())
+    if input.light_level() > 6:
+        light.clear()
+    elif input.light_level() < 6:
+        light.set_all(light.rgb(0, 255, 0))
+    else:
+        light.set_all(light.rgb(255, 255, 0))
+    
---
+++ main.ts
@@ -0,0 +1,11 @@
+while (true) {
+    console.log("Light Level:" + input.lightLevel())
+    if (input.lightLevel() > 6) {
+        light.clear()
+    } else if (input.lightLevel() < 6) {
+        light.setAll(light.rgb(0, 255, 0))
+    } else {
+        light.setAll(light.rgb(255, 255, 0))
+    }
+    
+}
